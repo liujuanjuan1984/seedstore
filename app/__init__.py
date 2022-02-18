9ea8ad9c41f0b554924a41c42080436f2f9fe160
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-02-18 18:47:39 +0800

    try:bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bootstrap import Bootstrap
 from config import config
 
 db = SQLAlchemy()
@@ -18,6 +18,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     db.init_app(app)
+    bootstrap = Bootstrap(app)
 
     from .main import main as main_blueprint
 
