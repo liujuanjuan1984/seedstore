c5936af2124d35ab8b98894c8308dd240fecafe6
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-05-11 20:38:00 +0800

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_login import LoginManager
+#from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
